bbdcadc3889bd8097e2366328e90fd699eeec27e
Atharv Phadnis <athiphadnis@gmail.com>
2021-07-22 00:19:55 +0530

    Tinted 3*3 Blocks in dual colors

--- src/Line.js
+++ src/Line.js
@@ -4,14 +4,16 @@ export default function Line(props) {
 	return (
 		<div>
 			<Row className="h-50 w-350 w-md-450">
-				{[0, 1, 2, 3, 4, 5, 6, 7, 8].map(value =>
+				{[0, 1, 2, 3, 4, 5, 6, 7, 8].map(col =>
 					{
-						const empty = props.empty[props.row].includes(value)
-						const className = "h-full bg-input text-center border-0 font-size-18 font-weight-bold " + (empty ? "text-danger" : "text-readonly" )
-						return (<Col key={value} className="p-5">
+						const empty = props.empty[props.row].includes(col)
+						const className = "h-full text-center border-0 font-size-18 font-weight-bold " +
+							(empty ? "text-user " : "text-readonly " ) + ( (Math.floor(props.row / 3) +
+							Math.floor(col / 3)) % 2 === 0 ? "bg-light-input" : "bg-input" )
+						return (<Col key={col} className="p-5">
 							<Input
-								value={props.data[value]}
-								{...(empty ? {onChange: (e) => props.update(e, props.row, value)} : {readOnly: true} )}
+								value={props.data[col]}
+								{...(empty ? {onChange: (e) => props.update(e, props.row, col)} : {readOnly: true} )}
 								className={className}
 								min="1"
 								max="9"
