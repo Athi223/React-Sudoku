9b7b92146832dd69b140a6fd6e915a07ebe224a5
Atharv Phadnis <athiphadnis@gmail.com>
2021-07-21 15:39:09 +0530

    Created Basic Board with Default & User Inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { PageWrapper, ContentWrapper, Input, Card, CardTitle, Row, Col, Button } from "reacthalfmoon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Block(props) {
+	return (
+		<div>
+			<Row className="h-50">
+				<Col className="p-5">
+					<Input readOnly value={props.value} className="h-full bg-input text-readonly text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+			</Row>
+			<Row className="h-50">
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+			</Row>
+			<Row className="h-50">
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+				<Col className="p-5">
+					<Input className="h-full bg-input text-danger text-center border-0 font-size-18 font-weight-bold" min="1" max="9" type="number"></Input>
+				</Col>
+			</Row>
+		</div>
+	)
 }
 
-export default App;
+export default function App() {
+	return (
+		<PageWrapper>
+			<ContentWrapper className="h-full d-flex justify-content-center align-items-center">
+				<Card style={{ width: "max-content" }} className="text-center">
+					<CardTitle className="text-primary mb-10"><h2>Sudoku</h2></CardTitle>
+					<Row className="h-150 w-450">
+						<Col><Block value={1} /></Col>
+						<Col><Block value={2} /></Col>
+						<Col><Block value={3} /></Col>
+					</Row>
+					<Row className="h-150 w-450">
+						<Col><Block value={4} /></Col>
+						<Col><Block value={5} /></Col>
+						<Col><Block value={6} /></Col>
+					</Row>
+					<Row className="h-150 w-450">
+						<Col><Block value={7} /></Col>
+						<Col><Block value={8} /></Col>
+						<Col><Block value={9} /></Col>
+					</Row>
+					<div className="d-flex justify-content-around mt-20">
+						<Button color="danger" size="lg">Reset</Button>
+						<Button color="secondary" size="lg">New game</Button>
+						<Button color="success" size="lg">Check</Button>
+					</div>
+				</Card>
+			</ContentWrapper>
+		</PageWrapper>
+	)
+}
